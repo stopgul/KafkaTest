2fd7f8e58276b9190ee0824c7e7fababd588faac
stopgul <sinantopgul@windowslive.com>
2019-03-18 08:34:48 -0400

    initial commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+              {
+                  { "group.id", "messageConsumer" }
+                  ,{ "bootstrap.servers", "localhost:9092" }
+                  ,{ "enable.auto.commit", "false"}
+              };
+
+            using (var consumer = new Consumer<Null, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                consumer.Subscribe(new string[] { "topic-royal-2" });
+
+                consumer.OnMessage += (_, msg) =>
+                {
+                    Console.WriteLine($"Message: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+                    consumer.CommitAsync(msg);
+                };
+
+                while (true)
+                {
+                    consumer.Poll(100);
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:9092" }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                string text = null;
+
+                while (text != "quit")
+                {
+                    Console.Write("Add Message: ");
+                    text = Console.ReadLine();
+                    producer.ProduceAsync("topic-royal-2", null, text);
+                }
+                producer.Flush(100);
+            }
+        }
+    }
+}
